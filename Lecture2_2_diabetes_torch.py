40a0a314d758b4adca68f5c3a7c2e0111330008b
Gyeong-hoon Lee <57140818+ghlee3401@users.noreply.github.com>
2023-02-08 13:08:22 +0900

    Update Lecture2_2_diabetes_torch.py

--- Lecture2_2_diabetes_torch.py
+++ Lecture2_2_diabetes_torch.py
@@ -3,6 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_diabetes
 
+# 이전 수업에서 했던 그림 그리는 것을 함수로 만들어 사용합니다. 
+def plot(x, y, w, b, step, min_val=-0.1, max_val=0.15, name="plot"):
+    plt.scatter(x, y) 
+    plt.xlabel("x")
+    plt.ylabel("y")
+    point1 = (min_val, min_val * w.data.numpy() + b.data.numpy()) # x값이 적당히 작을때 직선 위의 점
+    point2 = (max_val, max_val * w.data.numpy() + b.data.numpy()) # x값이 적당히 클때 직선 위의 점
+    plt.plot([point1[0], point2[0]], [point1[1], point2[1]], 'r')
+    plt.grid() 
+    plt.savefig(name+"{}.png".format(step)) # 초기값일 때의 직선을 그립니다. 
+    plt.close() 
+
 torch.manual_seed(777)
 np.random.seed(777)
 
@@ -20,6 +32,7 @@ b = torch.ones([1], requires_grad=True)
 optimizer = torch.optim.SGD([w, b], lr=1e-2)
 loss_fn = torch.nn.MSELoss()
 
+# 1회 어떻게 작동하는지 체크하는 코드입니다. 
 # y_hat = xt[0] * w + b 
 # loss = loss_fn(y_hat, yt[0])
 
@@ -27,44 +40,39 @@ loss_fn = torch.nn.MSELoss()
 # loss.backward() 
 # optimizer.step() 
 
-def plot(x, y, w, b, step):
-    plt.scatter(x, y) 
-    plt.xlabel("x")
-    plt.ylabel("y")
-    point1 = (-0.1, -0.1 * w.data.numpy() + b.data.numpy()) # x값이 적당히 작을때 직선 위의 점
-    point2 = (0.15, 0.15 * w.data.numpy() + b.data.numpy()) # x값이 적당히 클때 직선 위의 점
-    plt.plot([point1[0], point2[0]], [point1[1], point2[1]], 'r')
-    plt.grid() 
-    plt.savefig("lec2_2_{}.png".format(step)) # 초기값일 때의 직선을 그립니다. 
-    plt.close() 
-
+# 모든 데이터 셋에 대해서 해봅시다. 
 cnt = 0 
-idx = [i for i in range(len(x))]
+idx = [i for i in range(len(x))]  # 이 문법에 대해서 찾아보세요!
+
+"""
+idx = [i for i in range(len(x))] 
+위 문장은 아래 for문과 같은 의미입니다 
+
+idx = list() 
+for i in range(len(x)):
+    idx.append(i)
+"""
 for k in range(100):
-    idx = np.random.permutation(idx)
+    idx = np.random.permutation(idx)  
+    # permutaion은 안에 순서를 바꾸어줍니다. 
+    epoch_loss = 0
     for i in idx:
+        """ 
+        idx의 순서가 바뀌어 들어간다는 것은 epoch마다 학습에 사용되는 샘플의 순서가 바뀐다는 뜻입니다. 
+        """
         y_hat = xt[i] * w + b
         loss = loss_fn(y_hat, yt[i].unsqueeze(0))
 
+        epoch_loss += loss.data.numpy()
         optimizer.zero_grad()
         loss.backward() 
         optimizer.step() 
+    
+    epoch_loss = epoch_loss / len(idx)
+    print("Epoch : {}, Loss : {}".format(k, epoch_loss))
         
     if k % 20 == 0:
-        plot(x, y, w, b, cnt)
+        plot(x, y, w, b, cnt, name="lec2_2_")
         cnt += 1
+        
 
-# plt.scatter(x, y) 
-# plt.xlabel("x")
-# plt.ylabel("y")
-# point1 = (-0.1, -0.1 * w + b) # x값이 적당히 작을때 직선 위의 점
-# point2 = (0.15, 0.15 * w + b) # x값이 적당히 클때 직선 위의 점
-# plt.plot([point1[0], point2[0]], [point1[1], point2[1]], 'r')
-# plt.grid() 
-# plt.savefig("lec2_1_3.png") # 초기값일 때의 직선을 그립니다. 
-# plt.close() 
-
-# x = 0.18
-# y_pred = x * w + b
-
-# print(y_pred)
